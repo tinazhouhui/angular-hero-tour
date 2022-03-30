2a85e6f8b3f9319d2abf8e97ca6057117ca91816
Tina <tina.zhou.hui@gmail.com>
2022-03-30 13:00:04 +0100

    Fix tests

--- hero-tour/src/app/app.component.spec.ts
+++ hero-tour/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'hero-tour'`, () => {
+  it(`should have as title 'Tour of Heroes'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('hero-tour');
+    expect(app.title).toEqual('Tour of Heroes');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('hero-tour app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Tour of Heroes');
   });
 });
