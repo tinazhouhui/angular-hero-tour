39ba037969d27e1f2a23ef8c612e250f042297f1
Tina <tina.zhou.hui@gmail.com>
2022-03-30 16:47:09 +0100

    dashboard functionality

--- hero-tour/src/app/dashboard/dashboard.component.ts
+++ hero-tour/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Hero} from "../interfaces/hero";
+import {HeroService} from "../services/hero/hero.service";
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  heroes: Hero[] = []
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()  //todo isnt calling this twice expensive?
+      .subscribe(heroes => this.heroes = heroes.slice(1,5))
   }
 
 }
