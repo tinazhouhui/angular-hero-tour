f7a8880d6d83b8f8f92b680a65d975a67458e3cd
Tina <tina.zhou.hui@gmail.com>
2022-03-30 13:15:12 +0100

    service and dependency injection

--- hero-tour/src/app/heroes/heroes.component.ts
+++ hero-tour/src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Hero} from '../interfaces/hero'
-import {HEROES} from "../mock-heroes";
+import {HeroService} from "../hero.service";
 
 @Component({  // decorator function that specifies the Angular metadata for the component
   selector: 'app-heroes',  // the component's CSS element selector
@@ -8,14 +8,18 @@ import {HEROES} from "../mock-heroes";
   styleUrls: ['./heroes.component.scss']  // the location of the component's private CSS styles
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES
+  heroes: Hero[] = [];
   selectedHero?: Hero;
 
-  constructor() {
-  }
+  constructor(private heroService: HeroService) { }
+  // do I not need to assign to a this? also what if I have like 10 services?
 
   ngOnInit(): void {
+    this.getHeroes()
   }
+   getHeroes(): void {
+      this.heroes = this.heroService.getHeroes()
+    }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero
