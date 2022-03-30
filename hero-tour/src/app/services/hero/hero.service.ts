752eeb800d9ddb65c74b642fdb2889d3d253a634
Tina <tina.zhou.hui@gmail.com>
2022-03-30 18:58:07 +0100

    cleanup

--- hero-tour/src/app/services/hero/hero.service.ts
+++ hero-tour/src/app/services/hero/hero.service.ts
@@ -60,6 +60,7 @@ export class HeroService {
   }
 
   addHero(hero: Hero): Observable<Hero> {
+    console.log(hero)
     return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions)
       .pipe(
         tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
